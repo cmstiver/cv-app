7816a33ec2ccca14907af477485f9e5967223ac9
Chayse Stiver <cmstiver1@gmail.com>
2021-08-01 00:54:17 -0700

    created css template for forms and created general info form

--- src/components/Form/General.js
+++ src/components/Form/General.js
@@ -3,21 +3,35 @@ import React from 'react';
 function General() {
   return (
     <div id="general">
-      <div id="general-container">
-        <span>First Name: </span>
-        <input id="firstname" type="text" />
-        <span>Last Name: </span>
-        <input id="lastname" type="text" />
-        <span>Email: </span>
-        <input id="email" type="text" />
-        <span>Phone #: </span>
-        <input id="phone" type="text" />
-        <span>Location: </span>
-        <input id="location" type="text" />
-        <span>Professional Title (optional): </span>
-        <input id="title" type="text" />
-        <span>Professional Summary (optional): </span>
-        <input id="summary" type="text" />
+      <div id="general-form" className="form">
+        <div>
+          <p>First Name: </p>
+          <input id="firstname" type="text" />
+        </div>
+        <div>
+          <p>Last Name: </p>
+          <input id="lastname" type="text" />
+        </div>
+        <div>
+          <p>Email: </p>
+          <input id="email" type="text" />
+        </div>
+        <div>
+          <p>Phone #: </p>
+          <input id="phone" type="text" />
+        </div>
+        <div>
+          <p>Location: </p>
+          <input id="location" type="text" />
+        </div>
+        <div>
+          <p>Professional Title (optional): </p>
+          <input id="title" type="text" />
+        </div>
+        <div>
+          <p>Professional Summary (optional): </p>
+          <textarea id="summary" />
+        </div>
       </div>
     </div>
   );
