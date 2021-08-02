1d2985f351179276c1e1e1c2d3b0c4d25aeef3aa
Chayse Stiver <cmstiver1@gmail.com>
2021-08-02 13:16:08 -0700

    styling changes

--- src/App.js
+++ src/App.js
@@ -172,7 +172,7 @@ class App extends Component {
           key: uniqid(),
           school: 'University of Washington',
           city: 'Tacoma, WA',
-          degree: 'Working Towards B.A.',
+          degree: 'Working Towards B.A. Computer Science ',
           date: '2021 - Present',
         },
       ],
